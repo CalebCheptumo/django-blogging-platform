9710745bcbc23a732967c3c4595e3d34870a35bd
calebcheptumo <ckibusia@gmail.com>
2023-11-13 13:41:51 +0300

    added  user authentication, form and views for creating blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import blog_post
+from .views import blog_post , create_blog_post
 
 
 
 urlpatterns = [
     path('posts/', blog_post, name='blog_posts'),
+    path('create/', create_blog_post, name='create_blog_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from .models import BlogPost
+from .forms import BlogPostForm
 
-# Create your views here.
+
+@login_required
+def create_blog_post(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect(blog_post)
+    else:
+            form = BlogPostForm()
+    return render(request, 'blog/create_blog_post.html',{'form' : form})
+
+
+
+
 def blog_post(request):
     posts =BlogPost.objects.all()
     return render(request,'blog/blog_posts.html',{'posts':posts})
